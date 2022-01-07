ebe959ffab570facc1596eca1a5696fba75358e0
Divakar Mishra <divakar.mishra@comm100.com>
2022-01-07 15:40:40 +0530

    command line arguments

--- SearchAndReplace.py
+++ SearchAndReplace.py
@@ -1,7 +1,12 @@
 import os
+import sys
 
 from filereader import search_replace
 
+var_name = sys.argv[1]
+var_value = sys.argv[2]
+
+os.environ[var_name] = var_value
 plugin_name = os.environ['plugin_name']
 search_replace(plugin_name)
 
